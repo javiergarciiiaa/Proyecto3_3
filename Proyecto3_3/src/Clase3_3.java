3ce3263c6d8c8b1dceb08740d7401877107bbc8d
javiergarciiiaa <fcojaviergarciamartinez@outlook.es>
2023-11-29 20:32:36 +0100

    a

--- Proyecto3_3/src/Clase3_3.java
+++ Proyecto3_3/src/Clase3_3.java
@@ -3,7 +3,7 @@ public class Clase3_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Proyecto3_3 Francisco Javier Garcia");
+		System.out.println("Canela En Rama");
 	}
 
 }
